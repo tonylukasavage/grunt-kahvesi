7553ad36cd3fff604431806fcabc44d627955769
Tony Lukasavage <anthony.lukasavage@gmail.com>
2014-10-15 15:48:02 -0400

    fix istanbul execution, manage excludes

--- tasks/kahvesi.js
+++ tasks/kahvesi.js
@@ -9,6 +9,7 @@
 'use strict';
 
 var exec = require('child_process').exec,
+  format = require('util').format,
   path = require('path');
 
 var BIN = path.join(__dirname, '..', 'node_modules', '.bin');
@@ -17,20 +18,25 @@ module.exports = function(grunt) {
 
   grunt.registerMultiTask('kahvesi', 'grunt plugin for generating istanbul + mocha coverage reports', function() {
     var done = this.async(),
-      cmd = '"' + BIN + '/istanbul" cover --report html "' + BIN + '/_mocha" -- -R min ' +
-        this.filesSrc.reduce(function(p,c) { return (p || '') + ' "' + c + '" '; });
+      istanbul = quote(BIN + '/istanbul'),
+      mocha = quote(BIN + '/_mocha'),
+      files = this.filesSrc.reduce(function(p,c) { return (p || '') + ' "' + c + '" '; }),
+      excludes = ['**/node_modules/**', '**/test/mocha/test/**'],
+      args = process.env.KAHVESI_TEST ? '--no-default-excludes -x ' + quote(excludes.join(' ')) : '',
+      cmd = format('%s cover --report html %s %s -- -R min %s', istanbul, args, mocha, files);
 
-    grunt.log.writeln(cmd);
+    grunt.log.debug(cmd);
     exec(cmd, function(err, stdout, stderr) {
       if (err) { grunt.fail.fatal(err); }
       if (/No coverage information was collected/.test(stdout)) {
         grunt.fail.warn('No coverage information was collected. Report not generated.');
       } else {
-        grunt.log.ok(stdout);
-        //grunt.log.ok('test coverage report generated to "./coverage/index.html"');
+        grunt.log.ok('test coverage report generated to "./coverage/index.html"');
       }
       done();
     });
   });
 
 };
+
+function quote(str) { return '"' + str + '"'; }
