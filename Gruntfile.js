847e67d7c3f977bd0eca4f58375d524f443b4582
Tony Lukasavage <anthony.lukasavage@gmail.com>
2014-12-10 09:37:53 -0500

    fix whitespacing

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,64 +10,64 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
+	// Project configuration.
+	grunt.initConfig({
 
-    // set test env
-    env: {
-      test: {
-        KAHVESI_TEST: '1'
-      }
-    },
+		// set test env
+		env: {
+			test: {
+				KAHVESI_TEST: '1'
+			}
+		},
 
-    // lint the code
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>',
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-      },
-    },
+		// lint the code
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'tasks/*.js',
+				'<%= nodeunit.tests %>',
+			],
+			options: {
+				jshintrc: '.jshintrc',
+			},
+		},
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp'],
-    },
+		// Before generating any new files, remove any previously-created files.
+		clean: {
+			tests: ['tmp'],
+		},
 
-    // Configuration to be run (and then tested).
-    kahvesi: {
-      single_test: {
-        src: ['test/mocha/test/test1_test.js']
-      },
-      multiple_test: {
-        src: ['test/mocha/test/**/*_test.js']
-      }
-    },
+		// Configuration to be run (and then tested).
+		kahvesi: {
+			single_test: {
+				src: ['test/mocha/test/test1_test.js']
+			},
+			multiple_test: {
+				src: ['test/mocha/test/**/*_test.js']
+			}
+		},
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
+		// Unit tests.
+		nodeunit: {
+			tests: ['test/*_test.js'],
+		},
 
-  });
+	});
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+	// Actually load this plugin's task(s).
+	grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-env');
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	grunt.loadNpmTasks('grunt-env');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'env', 'kahvesi', 'nodeunit']);
+	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
+	// plugin's task(s), then test the result.
+	grunt.registerTask('test', ['clean', 'env', 'kahvesi', 'nodeunit']);
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+	// By default, lint and run all tests.
+	grunt.registerTask('default', ['jshint', 'test']);
 
 };
--- test/kahvesi_test.js
+++ test/kahvesi_test.js
@@ -3,22 +3,22 @@
 var grunt = require('grunt');
 
 exports.kahvesi = {
-  single_test: function(test) {
-    test.expect(4);
-    test.ok(grunt.file.exists('coverage'));
-    test.ok(grunt.file.exists('coverage/index.html'));
-    test.ok(grunt.file.exists('coverage/coverage.json'));
-    test.ok(grunt.file.exists('coverage/lib/test1.js.html'));
-    test.done();
-  },
-  multiple_test: function(test) {
-    test.expect(6);
-    test.ok(grunt.file.exists('coverage'));
-    test.ok(grunt.file.exists('coverage/index.html'));
-    test.ok(grunt.file.exists('coverage/coverage.json'));
-    test.ok(grunt.file.exists('coverage/lib/test1.js.html'));
-    test.ok(grunt.file.exists('coverage/lib/test2.js.html'));
-    test.ok(grunt.file.exists('coverage/lib/more/test3.js.html'));
-    test.done();
-  },
+	single_test: function(test) {
+		test.expect(4);
+		test.ok(grunt.file.exists('coverage'));
+		test.ok(grunt.file.exists('coverage/index.html'));
+		test.ok(grunt.file.exists('coverage/coverage.json'));
+		test.ok(grunt.file.exists('coverage/lib/test1.js.html'));
+		test.done();
+	},
+	multiple_test: function(test) {
+		test.expect(6);
+		test.ok(grunt.file.exists('coverage'));
+		test.ok(grunt.file.exists('coverage/index.html'));
+		test.ok(grunt.file.exists('coverage/coverage.json'));
+		test.ok(grunt.file.exists('coverage/lib/test1.js.html'));
+		test.ok(grunt.file.exists('coverage/lib/test2.js.html'));
+		test.ok(grunt.file.exists('coverage/lib/more/test3.js.html'));
+		test.done();
+	},
 };
